a52569455fb5de5b622aea4af839e99a71c76ed8
Sharad Kumar Singh <singhsharad529@gmail.com>
2023-04-04 23:58:25 +0530

    added header for cors

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -11,6 +11,19 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://mynotemate.netlify.app/"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    ); // If needed
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    ); // If needed
+
     res.json({
       _id: user._id,
       name: user.name,
