cd7d461751a3c4de5a47231a816abec5b3e4487a
Sharad Kumar Singh <singhsharad529@gmail.com>
2023-12-12 21:18:56 +0530

    correction in jwt token return function

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,5 +1,12 @@
 const jwt = require("jsonwebtoken");
 
+/*
+
+- This is file where we are generating jwt token 
+- combining id and jwt secret and returning jwt token
+
+*/
+
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "1d",
